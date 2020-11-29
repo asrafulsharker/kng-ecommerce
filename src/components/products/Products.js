0ab71dad363c6c8e440c66aaf753cde0f2603e77
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-11-29 19:34:09 +0600

    add product component

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,20 +1,27 @@
+  
 import React from 'react';
-import Grid from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
-const products =[
-    { id: 1, name:'Shoes',description: 'Running shoes.'},
-    { id: 2, name:'Macbook',description: 'Apple macbook.'},
-]
+import Product from './Product/Product';
+import useStyles from './styles';
 
-const Products = ()=>{
-    <main>
-        <Grid container justify="center" spacing={4}>
-            {products.map((product)=>{
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    
-                 </Grid>
-            })}
-        </Grid>
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
+
+  if (!products.length) return <p>Loading...</p>;
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
     </main>
-}
+  );
+};
+
 export default Products;
